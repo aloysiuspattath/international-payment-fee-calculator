3512a1d54f32c1997e9858c44f7985f2faa7f9c0
aloysiuspattath <aloysiuspattath@gmail.com>
2026-06-20 22:47:29 +0530

    chore: update canonical URL to fees.techfliq.com

--- scripts/build.js
+++ scripts/build.js
@@ -18,7 +18,7 @@ const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 const template = fs.readFileSync(templatePath, 'utf8');
 
 const countries = Object.keys(config.stripe);
-const baseUrl = 'https://payfeecalc.techfliq.com'; // Custom domain
+const baseUrl = 'https://fees.techfliq.com'; // Custom domain
 
 let sitemapUrls = [];
 
